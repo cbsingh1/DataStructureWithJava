19aca5024e9cefcb0c19262f7274dc1657e82a63
cbsingh1 <cbhansingh1@gmail.com>
2022-02-18 15:39:57 +0800

    Sub String counter - 2 Solutions

---
+++ src/main/java/com/cbsingh/string/SubStringCounter.java
@@ -0,0 +1,30 @@
+package com.cbsingh.string;
+
+public class SubStringCounter {
+
+    public static int countSubString(String str, String findStr) {
+        int lastIndex = 0;
+        int count = 0;
+
+        while(lastIndex != -1){
+            lastIndex = str.indexOf(findStr,lastIndex);
+            if(lastIndex != -1){
+                count ++;
+                lastIndex += findStr.length();
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        String str= "abcdabcdabc";
+        String findStr = "abc";
+
+        //Normal solution
+        System.out.println(countSubString(str, findStr));
+
+        //Using String regex
+        System.out.println(str.split(findStr, -1).length);
+
+    }
+}
