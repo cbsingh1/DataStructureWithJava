ab1c368cc0326744496ef8ca2294cb127668d028
cbsingh1 <cbhansingh1@gmail.com>
2022-02-19 22:56:23 +0800

    Shell Sort implementaion with Driver program

--- src/main/java/com/cbsingh/sorting/ShellSortImpl.java
+++ src/main/java/com/cbsingh/sorting/ShellSortImpl.java
@@ -3,24 +3,18 @@ package com.cbsingh.sorting;
 import java.util.Arrays;
 
 public class ShellSortImpl {
-
     static void shellSort(int[] arr) {
-
         for (int gap = arr.length / 2; gap > 0; gap /= 2) {
-
             for (int i = gap; i < arr.length; i++) {
                 int newElement = arr[i];
                 int j = i;
-
                 while (j >= gap && arr[j - gap] > newElement) {
                     arr[j] = arr[j - gap];
                     j -= gap;
                 }
                 arr[j] = newElement;
             }
-
         }
-
     }
 
     public static void main(String[] args) {
