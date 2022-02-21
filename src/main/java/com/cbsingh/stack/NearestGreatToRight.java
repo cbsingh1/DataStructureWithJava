6adff0d6bdb9d7865d9f7285d762143fb2204af2
cbsingh1 <cbhansingh1@gmail.com>
2022-02-21 18:37:18 +0800

    Nearest great to Right using Stack

--- src/main/java/com/cbsingh/stack/NearestGreatToRight.java
+++ src/main/java/com/cbsingh/stack/NearestGreatToRight.java
@@ -1,5 +1,8 @@
 package com.cbsingh.stack;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 public class NearestGreatToRight {
 
     //Solution 1 - Using Brute Force (Time complexity - O(n2)
@@ -17,8 +20,41 @@ public class NearestGreatToRight {
         }
     }
 
+
+    public static void nearestGreatToRightUsingStack(int arr[]) {
+
+        int [] tempArr = new int[arr.length];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = arr.length-1; i >=0 ; i--) {
+
+            if(stack.empty()) {
+                tempArr[i] = -1;
+            } else {
+
+                if (arr[i] > stack.peek()) {
+                    while (!stack.empty()) {
+                        stack.pop();
+
+                        if(arr[i] < stack.peek()) {
+                            tempArr[i] = stack.peek();
+                            break;
+                        }
+                    }
+                } else {
+                    tempArr[i] = stack.peek();
+                }
+            }
+            stack.push(arr[i]);
+        }
+
+        System.out.println(Arrays.toString(tempArr));
+    }
+
     public static void main(String[] args) {
         int arr[] = {20, 35, -15, 7, 55, 1, -22};
+
         nearestGreatToRightBruteForce(arr);
+
+        nearestGreatToRightUsingStack(arr);
     }
 }
