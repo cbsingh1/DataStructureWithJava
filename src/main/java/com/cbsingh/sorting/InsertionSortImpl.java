1bf68c72712e2ec4e0dc437d9fcd166a1a445bbc
cbsingh1 <cbhansingh1@gmail.com>
2022-02-25 17:13:55 +0800

    Insertion Sort Implementation using Recursion

--- src/main/java/com/cbsingh/sorting/InsertionSortImpl.java
+++ src/main/java/com/cbsingh/sorting/InsertionSortImpl.java
@@ -6,20 +6,35 @@ public class InsertionSortImpl {
 
     public static void insertionSort(int arr[]) {
 
-        for (int i = 1; i < arr.length; i++) {
-            int currElement = arr[i];
-            int j;
-            for (j = i; j > 0 && arr[j - 1] > currElement; j--) {
-                arr[j] = arr[j - 1];
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < arr.length; firstUnsortedIndex++) {
+            int currElement = arr[firstUnsortedIndex];
+            int i;
+            for (i = firstUnsortedIndex; i > 0 && arr[i - 1] > currElement; i--) {
+                arr[i] = arr[i - 1];
             }
-            arr[j] = currElement;
+            arr[i] = currElement;
         }
+    }
+
+    public static void insertionSortRecursiveApproach(int input[], int numItems) {
+        if(numItems < 2)
+            return;
+
+        insertionSortRecursiveApproach(input, numItems-1);
+
+        int newElement = input[numItems-1];
+        int j;
+        for (j = numItems-1; j > 0 && input[j-1] > newElement; j--) {
+            input[j]=input[j-1];
+        }
+        input[j] = newElement;
     }
 
     //Driver program for testing
     public static void main(String[] args) {
         int arr[] = {20, 35, -15, 7, 55, 1, -22};
-        insertionSort(arr);
+        //insertionSort(arr);
+        insertionSortRecursiveApproach(arr, arr.length);
         System.out.println(Arrays.toString(arr));
     }
 }
