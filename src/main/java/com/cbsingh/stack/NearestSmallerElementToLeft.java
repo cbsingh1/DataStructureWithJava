be2db38662d42e6e4af2c9f503d17f9fa2eaa148
cbsingh1 <cbhansingh1@gmail.com>
2022-03-07 16:30:10 +0800

    Nearest Smaller element to Left using Stack & Brute Force

---
+++ src/main/java/com/cbsingh/stack/NearestSmallerElementToLeft.java
@@ -0,0 +1,52 @@
+package com.cbsingh.stack;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class NearestSmallerElementToLeft {
+
+    static void nearestSmalltoLeftUsingBruteForce(int[] arr) {
+        int []tempArr = new int[arr.length];
+
+        //Here i is started with index 0, to fill first element with -1.
+        // If -1 is not needed, then i can be started from index 1.
+        for (int i = 0; i < arr.length ; i++) {
+            tempArr[i] = -1;
+            for (int j = i-1; j >= 0; j--) {
+                if(arr[j] < arr[i]) {
+                    tempArr[i] = arr[j];
+                    break;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(tempArr));
+    }
+
+    private static void nearestSmalltoLeftUsingStack(int[] arr) {
+        int []tempArr = new int[arr.length];
+        Stack<Integer> stack = new Stack<>();
+
+        //Here i is started with index 0, to fill first element with -1.
+        // If -1 is not needed, then i can be started from index 1.
+        for (int i = 0; i < arr.length ; i++) {
+            tempArr[i] = -1;
+
+            while (!stack.isEmpty() && stack.peek() > arr[i]) {
+                stack.pop();
+            }
+
+            if(!stack.isEmpty()) {
+                tempArr[i] = stack.peek();
+            }
+            stack.push(arr[i]);
+        }
+        System.out.println(Arrays.toString(tempArr));
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {4,5,2,10,8, 1, 3};
+
+        //nearestSmalltoLeftUsingBruteForce(arr);
+        nearestSmalltoLeftUsingStack(arr);
+    }
+}
