7952b91071b24db53716f17d96cbba5608ace13b
cbsingh1 <cbhansingh1@gmail.com>
2022-03-19 09:46:36 +0800

    Nearest Smaller to Right

---
+++ src/main/java/com/cbsingh/stack/NearestSmallerToRight.java
@@ -0,0 +1,53 @@
+package com.cbsingh.stack;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class NearestSmallerToRight {
+
+    public static void main(String[] args) {
+        int []arr = {4,5,2,10,8,9,7};
+
+        //findUsingBruteForce(arr);
+        findUsingStack(arr);
+    }
+
+    private static void findUsingStack(int[] arr) {
+        int []tempArr = new int[arr.length];
+        Stack<Integer> stack = new Stack<>();
+
+        //Here, we are starting with last index,
+        // just to fill last element with -1,
+        // if not needed, then we can start with arr.length -2
+        for (int i = arr.length-1; i>=0; i--) {
+            tempArr[i] = -1;
+
+            while(!stack.isEmpty() && stack.peek() > arr[i]){
+                stack.pop();
+            }
+            if(!stack.isEmpty()) {
+                tempArr[i] = stack.peek();
+            }
+            stack.push(arr[i]);
+        }
+        System.out.println(Arrays.toString(tempArr));
+    }
+
+    private static void findUsingBruteForce(int[] arr) {
+
+        int []tempArr = new int[arr.length];
+
+        for (int i = 0; i < arr.length ; i++) {
+            tempArr[i] = -1;
+
+            for (int j = i+1; j < arr.length ; j++) {
+
+                if(arr[j] < arr[i]) {
+                    tempArr[i] = arr[j];
+                    break;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(tempArr));
+    }
+}
