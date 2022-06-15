298faffb16f2e39f8ace97f6fc65916bf4c8e666
cbsingh1 <cbhansingh1@gmail.com>
2022-06-15 23:51:41 +0800

    First Negative number in subarray of size K

---
+++ src/main/java/com/cbsingh/array/SlidingWindow/FirstNegativeNumInEveryWindowOfSizeK.java
@@ -0,0 +1,17 @@
+package com.cbsingh.array.SlidingWindow;
+
+public class FirstNegativeNumInEveryWindowOfSizeK {
+
+    public static void main(String[] args) {
+        int []arr = {2,3,5,2,9,7,1};
+        int subArraySize = 3;
+
+        System.out.println(firstNegativeNumInSubArray(arr, subArraySize));    
+    }
+
+    private static int firstNegativeNumInSubArray(int[] arr, int subArraySize) {
+        int returnVal = -1;
+
+        return returnVal;
+    }
+}
