1a322341355213df404a45f07328134d16f70660
cbsingh1 <cbhansingh1@gmail.com>
2022-06-18 08:05:17 +0800

    Count number of anagram in String

--- src/main/java/com/cbsingh/array/SlidingWindow/CountNumOfAnagram.java
+++ src/main/java/com/cbsingh/array/SlidingWindow/CountNumOfAnagram.java
@@ -1,90 +1,54 @@
 package com.cbsingh.array.SlidingWindow;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
+
+/**
+ *  We would create a Map of Pattern initially.
+ *  Then for each Substring, we will do changes in same hashmap.
+ *  Additionally, we will maintain a indicator to identify count of chars with more than zero value.
+ */
 public class CountNumOfAnagram {
 
-    public static void main(String[] args) {
-        String s = "eaylnlfdxf";
-        String pattern = "irc";
-
-        //System.out.println(countNumOfAnagramBruteForce(s, pattern));
-        //System.out.println(countNumOfAnagramUsingSubstring(s, pattern));
-        System.out.println(countNumOfAnagramWithSlidingWindow(s, pattern));
-    }
-
-    private static int countNumOfAnagramBruteForce(String s, String pattern) {
-        int count=0;
-        List<String> anagramList = new ArrayList<>();
-        anagramList.addAll(List.of("baaa", "abaa", "aaba", "aaab"));
-
-        for (int i = 0; i <s.length()-pattern.length() +1 ; i++) {
-            String tempStr = "";
-            for (int j = i; j < i+pattern.length(); j++)
-                tempStr += String.valueOf(s.charAt(j));
-
-            if(anagramList.contains(tempStr))
-                count++;
-        }
-        return count;
-    }
-
-    private static int countNumOfAnagramUsingSubstring(String s, String pattern) {
-        int count=0;
-
-        List<String> anagramList = new ArrayList<>();
-        anagramList.addAll(List.of("baaa", "abaa", "aaba", "aaab"));
-
-        for (int i = 0; i <s.length()-pattern.length() +1 ; i++) {
-            String temp = s.substring(i, i+pattern.length());
-            if(anagramList.contains(temp))
-                count++;
-        }
-
-        return count;
-    }
-
     private static int countNumOfAnagramWithSlidingWindow(String s, String pattern) {
-        int count=0, charCount, i=0, j=0;
-        char currentChar;
-        Map<Character, Integer> charCountMap = new HashMap<>();
-        Map<Character, Integer> patternMap = new HashMap<>();
+        int anagramCount=0, charCount, i=0, j=0, distinctCharsInMapValueMoreThanZero=0;
+        HashMap<Character, Integer> patternMap = new HashMap<>();
         for (int p = 0; p < pattern.length(); p++) {
-            currentChar = pattern.charAt(p);
-
-            if(patternMap.containsKey(currentChar)) {
-                charCount = patternMap.get(currentChar);
-                patternMap.put(currentChar, ++charCount);
-            } else
-                patternMap.put(currentChar, 1);
+            charCount = patternMap.getOrDefault(pattern.charAt(p), 0);
+            patternMap.put(pattern.charAt(p), ++charCount);
         }
+        distinctCharsInMapValueMoreThanZero = patternMap.size();
 
         while (j<s.length()) {
-            currentChar = s.charAt(j);
-            if(charCountMap.containsKey(currentChar)) {
-                charCount = charCountMap.get(currentChar);
-                charCountMap.put(currentChar, ++charCount);
-            } else
-                charCountMap.put(currentChar, 1);
+            if(patternMap.containsKey(s.charAt(j))) {
+                charCount = patternMap.get(s.charAt(j));
+                if(charCount==1)
+                    distinctCharsInMapValueMoreThanZero--;
+
+                patternMap.put(s.charAt(j), --charCount);
+            }
 
             if(j-i+1 == pattern.length()) {
-                if(patternMap.equals(charCountMap))
-                    count++;
+                if(distinctCharsInMapValueMoreThanZero == 0)
+                    anagramCount++;
 
-                if(charCountMap.containsKey(s.charAt(i))) {
-                    charCount = charCountMap.get(s.charAt(i));
-                    if(charCount > 1)
-                        charCountMap.put(s.charAt(i), --charCount);
-                    else
-                        charCountMap.remove(s.charAt(i));
+                if(patternMap.containsKey(s.charAt(i))) {
+                    charCount = patternMap.getOrDefault(s.charAt(i),0);
+                    if(charCount==0)
+                        distinctCharsInMapValueMoreThanZero++;
+
+                    patternMap.put(s.charAt(i), ++charCount);
                 }
                 i++;
             }
             j++;
         }
-        return count;
+        return anagramCount;
+    }
+
+    public static void main(String[] args) {
+        String s = "aabaabaa";
+        String pattern = "aaba";
+        System.out.println(countNumOfAnagramWithSlidingWindow(s, pattern));
     }
 }
