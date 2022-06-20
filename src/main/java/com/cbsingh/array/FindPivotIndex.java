4f4e496893725dd23bb5b0abd8610830a4d1364e
cbsingh1 <cbhansingh1@gmail.com>
2022-06-21 00:09:54 +0800

    Running Sum of an Array (Leetcode 1480)

---
+++ src/main/java/com/cbsingh/array/FindPivotIndex.java
@@ -0,0 +1,25 @@
+package com.cbsingh.array;
+
+public class FindPivotIndex {
+
+    private static int findPivotIndex(int[] nums) {
+        int sum=0, leftSum=0;
+        for(int num : nums)
+            sum += num;
+
+        for (int i = 0; i < nums.length; i++) {
+            if(leftSum == sum - leftSum - nums[i])
+                return i;
+
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+       System.out.println(findPivotIndex(new int[] {1,7,3,6,5,6}));
+       System.out.println(findPivotIndex(new int[] {2,-2,3}));
+       System.out.println(findPivotIndex(new int[] {2,1,-1}));
+       System.out.println(findPivotIndex(new int[] {1,2,3}));
+    }
+}
