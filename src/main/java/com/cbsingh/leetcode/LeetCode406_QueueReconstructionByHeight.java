df3064e7259c6967c1b1620c963c7257c0e7a4b9
cbsingh1 <cbhansingh1@gmail.com>
2022-06-30 23:57:18 +0800

    LeetCode406_QueueReconstructionByHeight

--- src/main/java/com/cbsingh/leetcode/LeetCode406_QueueReconstructionByHeight.java
+++ src/main/java/com/cbsingh/leetcode/LeetCode406_QueueReconstructionByHeight.java
@@ -2,25 +2,31 @@ package com.cbsingh.leetcode;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
 
 public class LeetCode406_QueueReconstructionByHeight {
     public static int[][] reconstructQueue(int[][] people) {
-        ArrayList<int[]> queue = new ArrayList<>();
+        Arrays.sort(people, (o1, o2) -> o1[0] == o2[0] ? o1[1] - o2[1] : o2[0] - o1[0]);
+        List<int[]> list = new ArrayList<>();
+        for(int []p : people)
+            list.add(p[1], p);
 
-        for (int i = 0; i < people.length; i++) {
-            if(queue.isEmpty())
-                queue.add(people[i]);
-            else {
-                if()
-            }
-        }
-
-        return queue.toArray(new int[][]{});
+        return list.toArray(new int[people.length][2]);
     }
+
 
     public static void main(String[] args) {
         int [][] people = new int[][]{{7,0}, {4,4},{7,1},{5,0},{6,1},{5,2} };
+        int [][] people1 = new int[][]{{6,0},{5,0}, {4,0},{3,2},{2,2},{1,4}};
+        //int [][] people = new int[][]{{7,0}, {4,4} };
 
-        System.out.println(Arrays.toString(reconstructQueue(people)));
+        int [][] output=  reconstructQueue(people1);
+
+        for (int i = 0; i < output.length; i++) {
+            System.out.println(Arrays.toString(output[i]));
+        }
+
     }
+
 }
