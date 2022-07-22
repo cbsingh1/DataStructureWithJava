e3e5d41722e3e34072f0d99e9836cb64e5bef7ec
cbsingh1 <cbhansingh1@gmail.com>
2022-07-22 19:27:20 +0800

    Sorting Array using recursion

---
+++ src/main/java/com/cbsingh/recursion/SortArrRecursion.java
@@ -0,0 +1,37 @@
+package com.cbsingh.recursion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class SortArrRecursion {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList(
+                Arrays.asList(3,2,1,4,5,6,7)
+        );
+        arr = sort(arr);
+        System.out.println(arr);
+    }
+
+    private static ArrayList sort(ArrayList nums) {
+        if(nums.size()==1) {
+            return nums;
+        }
+        int temp = (int) nums.get(nums.size()-1);
+        nums.remove(nums.size()-1);
+        nums = sort(nums);
+        nums = insert(nums, temp);
+        return nums;
+    }
+
+    private static ArrayList insert(ArrayList nums, int temp) {
+        if(nums.size()==0 || (int)nums.get(nums.size()-1) <= temp) {
+            nums.add(temp);
+            return nums;
+        }
+        int lastBigNum = (int) nums.get(nums.size()-1);
+        nums.remove(nums.size()-1);
+        nums = insert(nums, temp);
+        nums.add(lastBigNum);
+        return nums;
+    }
+}
