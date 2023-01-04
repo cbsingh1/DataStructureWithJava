ced6596e938b82e721978529dbf4a90f2b6ba618
Chandra Bhan Singh <cbhansingh1@gmail.com>
2023-01-04 19:54:47 +0800

    LeetCode 2244 Min Rount to complete all tasks

---
+++ src/main/java/com/cbsingh/leetcode/LeetCode2244_MinRoundToCompleteAllTask.java
@@ -0,0 +1,26 @@
+package com.cbsingh.leetcode;
+
+import java.util.*;
+
+public class LeetCode2244_MinRoundToCompleteAllTask {
+    public static void main(String[] args) {
+        System.out.println(minimumRounds(new int[] {2,2,3,3,2,4,4,4,4,4}));
+    }
+
+    public static int minimumRounds(int[] tasks) {
+        int answer=0;
+        Map<Integer, Integer> taskCountMap = new HashMap<>();
+        Arrays.stream(tasks)
+                .forEach(n -> taskCountMap.put(n, taskCountMap.getOrDefault(n, 0)+1));sr
+
+        for(int val : taskCountMap.values()) {
+            if(val==1) return -1;
+
+            if(val%3==0)
+                answer += val/3;
+            else
+                answer += val/3 + 1;
+        }
+        return answer;
+    }
+}
