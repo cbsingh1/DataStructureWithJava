e9998f97b4c09f2a1a1a31d3405632cdb6c4f8a9
Chandra Bhan Singh <cbhansingh1@gmail.com>
2025-04-21 22:26:47 +0800

    fix: optimize solution for leetcode 303 range sum query immutable using prefix sum

--- src/main/java/com/cbsingh/leetcode/Leetcode303_RangeSumQueryImmutable.java
+++ src/main/java/com/cbsingh/leetcode/Leetcode303_RangeSumQueryImmutable.java
@@ -10,11 +10,10 @@ public class Leetcode303_RangeSumQueryImmutable {
     int[] prefix;
 
     public Leetcode303_RangeSumQueryImmutable(int[] nums) {
-        prefix = new int[nums.length];
-        prefix[0]=nums[0];
         for(int i=1; i<nums.length; i++){
-            prefix[i] = prefix[i-1]+nums[i];
+            nums[i] += nums[i-1];
         }
+        this.prefix = nums;
     }
 
     public int sumRange(int left, int right) {
