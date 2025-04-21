97ce65df3762aa84d6330d3ebe5cc16a2c96d3ad
Chandra Bhan Singh <cbhansingh1@gmail.com>
2025-04-21 23:09:33 +0800

    chore: rename variables for leetcode 56o

--- src/main/java/com/cbsingh/leetcode/Leetcode560_SubarraySumEqualsK.java
+++ src/main/java/com/cbsingh/leetcode/Leetcode560_SubarraySumEqualsK.java
@@ -4,17 +4,17 @@ import java.util.HashMap;
 
 public class Leetcode560_SubarraySumEqualsK {
     //Time complexity : O(n) The entire nums array is traversed only once.
-    //Space complexity : O(n) Hashmap chainedHashTable can contain up to n distinct entries in the worst case.
+    //Space complexity : O(n) Hashmap can contain up to n distinct entries in the worst case.
     public int subarraySum(int[] nums, int k) {
-        int count = 0, sum = 0;
-        var map = new HashMap<Integer, Integer>();
-        map.put(0, 1);
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-            if (map.containsKey(sum - k)) {
-                count += map.get(sum - k);
+        int count = 0, prefixSum = 0;
+        var prefixSumFrequency = new HashMap<Integer, Integer>();
+        prefixSumFrequency.put(0, 1);
+        for (int num : nums) {
+            prefixSum += num;
+            if (prefixSumFrequency.containsKey(prefixSum - k)) {
+                count += prefixSumFrequency.get(prefixSum - k);
             }
-            map.put(sum, map.getOrDefault(sum, 0) + 1);
+            prefixSumFrequency.put(prefixSum, prefixSumFrequency.getOrDefault(prefixSum, 0) + 1);
         }
         return count;
     }
@@ -35,6 +35,6 @@ public class Leetcode560_SubarraySumEqualsK {
         var solution = new Leetcode560_SubarraySumEqualsK();
 
         var result = solution.subarraySum(new int[]{1, 1, 1}, 2);
-        assert result == 2;
+        System.out.println("Result : " + result);
     }
 }
